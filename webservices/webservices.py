346efa9119f0dd0baff513703a667873bc214686
Mohsen Kashi <mhmhkashi@gmail.com>
2020-10-31 22:25:16 +0100

    handle error on reading outdoor weather

--- webservices/webservices.py
+++ webservices/webservices.py
@@ -3,9 +3,13 @@ import json
 
 
 def getOutdoorTemprature():
-    weatherObject = check_output(
-        ['curl', 'api.openweathermap.org/data/2.5/weather?id=2753355&appid=2c7874d24ca6af575f6eae4a897583ff&units=metric'])
-    return int(json.loads(weatherObject)['main']['temp'])
+    try:
+        weatherObject = check_output(
+            ['curl',
+             'api.openweathermap.org/data/2.5/weather?id=2753355&appid=2c7874d24ca6af575f6eae4a897583ff&units=metric'])
+        return int(json.loads(weatherObject)['main']['temp'])
+    except:
+        return 99
 
 
 def farenheitToCelsius(f):
